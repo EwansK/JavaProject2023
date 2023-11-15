b7451567efab7efb7269d3533ce1b5dc7670da39
EwansK <ew.ewoldt@duocuc.cl>
2023-11-15 00:18:37 -0300

    JP(wip)

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ewans
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
